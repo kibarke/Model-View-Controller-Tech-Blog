098a427bd197951ee59073c53d671ebdb9d77c4d
kibarke <kibarkework@gmail.com>
2024-05-02 12:22:26 -0400

    looking at activity 21 review, copying code to get started

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+const express = require("express");
+
+const PORT = 3001;
 
 const app = express();
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
-app.get('/', (req, res) => {
-    res.render('home');
+app.listen(3001,() => {
+    console.log(`Listening on http://localhost:${PORT}`)
 });
-
-app.listen(3000);

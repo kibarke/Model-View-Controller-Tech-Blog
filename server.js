0b3dc00128ad3ae787a29286ea7b8de55f8e05a7
kibarke <kibarkework@gmail.com>
2024-05-02 12:46:30 -0400

    starter code

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require("express");
-
-const PORT = 3001;
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001,() => {
-    console.log(`Listening on http://localhost:${PORT}`)
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
